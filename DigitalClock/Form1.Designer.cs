25df37761d919d3f282990c091feadc8935803d9
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-26 23:27:04 +0100

    Update Form1.Designer.cs

--- DigitalClock/Form1.Designer.cs
+++ DigitalClock/Form1.Designer.cs
@@ -5,7 +5,7 @@ partial class Form1
         /// <summary>
         ///  Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         ///  Clean up any resources being used.
