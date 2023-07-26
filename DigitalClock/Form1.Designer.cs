ef8e0b66c247b8f517031fd7d03db5f4ca005e4a
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-26 23:28:52 +0100

    Update Form1.Designer.cs

--- DigitalClock/Form1.Designer.cs
+++ DigitalClock/Form1.Designer.cs
@@ -93,7 +93,7 @@ protected override void Dispose(bool disposing)
 
         private Label label1;
         private System.Windows.Forms.Timer timer1;
-        private Label label2;
+private Label label2;
 private Label label4;
 }
 }
