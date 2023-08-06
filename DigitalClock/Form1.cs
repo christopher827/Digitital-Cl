548d7b6107a636a51002d570b32e0fb601e9b2ab
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-06 04:39:33 +0100

    Update Form1.cs

--- DigitalClock/Form1.cs
+++ DigitalClock/Form1.cs
@@ -19,7 +19,7 @@ InitializeComponent();
 
 private void Form1_Load(object sender, EventArgs e)
 {
-            timer1.Start();
+timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
