fa2611715ae4db9ed8a1b7c0725773678aba1604
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-06 04:38:51 +0100

    Update Form1.cs

--- DigitalClock/Form1.cs
+++ DigitalClock/Form1.cs
@@ -14,7 +14,7 @@ public partial class Form1 : Form
 {
 public Form1()
 {
-            InitializeComponent();
+InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
