8134f5acc41572476cc613db700cdc7b1e2c669e
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-06 04:38:09 +0100

    Update Form1.cs

--- DigitalClock/Form1.cs
+++ DigitalClock/Form1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace DigitalClock
 {
-    public partial class Form1 : Form
+public partial class Form1 : Form
     {
         public Form1()
         {
